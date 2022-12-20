9adab9fa0ebc28df011cb00c7666305881a109f6
Franceschio <121029441+Franceschio@users.noreply.github.com>
2022-12-20 19:52:28 +0100

    esercizio 20-12-2022
    esercizio del 20-12-2022.

---
+++ 20-12-2022 es/script.js
@@ -0,0 +1,41 @@
+//for classico
+let numeri = [];
+
+for (let i = 0; i < 10; i++) {
+  numeri[i] = prompt("inserisci numero");
+  console.log(numeri[i]);
+}
+
+//for index
+// let numeri = [
+//   prompt("inserisci numero"),
+//   prompt("inserisci numero"),
+//   prompt("inserisci numero"),
+//   prompt("inserisci numero"),
+//   prompt("inserisci numero"),
+//   prompt("inserisci numero"),
+//   prompt("inserisci numero"),
+//   prompt("inserisci numero"),
+//   prompt("inserisci numero"),
+//   prompt("inserisci numero"),
+// ];
+// for (let numero in numeri) {
+//   console.log(numeri[numero]);
+// }
+
+//for value
+// let numeri = [
+//   prompt("inserisci numero"),
+//   prompt("inserisci numero"),
+//   prompt("inserisci numero"),
+//   prompt("inserisci numero"),
+//   prompt("inserisci numero"),
+//   prompt("inserisci numero"),
+//   prompt("inserisci numero"),
+//   prompt("inserisci numero"),
+//   prompt("inserisci numero"),
+//   prompt("inserisci numero"),
+// ];
+// for (let numero of numeri) {
+//   console.log(numero);
+// }
