e84696cae641a60cf5999e3aa6a11ff9c52ea6fc
Francesco Coppola <121029441+Franceschio@users.noreply.github.com>
2023-01-22 16:44:11 +0100

    Esercizio del 20-01-2023
    Esercizio del 20-01-2023

---
+++ 20-01-2023/funzioni.js
@@ -0,0 +1,71 @@
+//Comunicare col back-end
+
+function post(prod, genre) {
+  fetch(`https://api.escuelajs.co/api/v1/${genre}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(prod),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log("Risposta al POST ", data);
+      if (data.statusCode >= 400 && data.statusCode < 500) {
+        alert(
+          "qualcosa è andato storto, controlla che i dati inseriti siano corretti e ritenta."
+        );
+      } else {
+        alert("Inserimento riuscito con successo!");
+      }
+    })
+    .catch((error) => {
+      alert("Ci dispiace, è stato riscontrato un problema");
+      console.warn(error);
+    });
+}
+
+function delet(id, genre) {
+  fetch(`https://api.escuelajs.co/api/v1/${genre}/${id}`, {
+    method: "DELETE",
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log("Risposta al DELETE ", data);
+      if (data.statusCode >= 400 && data.statusCode < 500) {
+        alert("qualcosa è andato storto, controlla l'id inserito e ritenta.");
+      } else {
+        alert("Cancellazione riuscita con successo!");
+      }
+    })
+    .catch((error) => {
+      alert("Ci dispiace, è stato riscontrato un problema");
+      console.warn(error);
+    });
+}
+
+function put(id) {
+  fetch(`https://api.escuelajs.co/api/v1/products/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log("Risposta al Put ", data);
+      if (data.statusCode >= 400 && data.statusCode < 500) {
+        alert("qualcosa è andato storto, controlla i dati inseriti e ritenta.");
+      } else {
+        alert("Modifica riuscita con successo!");
+      }
+    })
+    .catch((error) => {
+      alert("Ci dispiace, è stato riscontrato un problema");
+      console.warn(error);
+    });
+}
+
+//Ho provato ad esportare ed importare le funzioni per la creazione delle card ma mi dava errore
+
+export { post, delet, put };
