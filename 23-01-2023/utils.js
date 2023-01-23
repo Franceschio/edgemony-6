fd450233f49f55d3b8bb947e59ab17ab2bfb41b9
Francesco Coppola <121029441+Franceschio@users.noreply.github.com>
2023-01-23 23:43:20 +0100

    Delete 23-01-2023 directory

--- 23-01-2023/utils.js
+++
@@ -1,5 +0,0 @@
-const cE = (type) => document.createElement(type);
-const qS = (element) => document.querySelector(element);
-const app = (type, element) => type.append(element);
-
-export { cE, qS, app };
